452167b4962292fff82c626d8ca047910dc8b894
Gopal <gopalmehtre10@gmail.com>
2026-01-14 02:05:16 +0530

    deployment issues

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -20,7 +20,7 @@ const register = async(req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
             maxAge: 7*24*60*1000
         });
 
@@ -50,7 +50,7 @@ const login = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
+            sameSite:process.env.NODE_ENV === 'production' ? 'none' : 'strict',
             maxAge: 7*24*60*60*1000
         });
 
@@ -70,7 +70,7 @@ const logout = (req, res) => {
         res.clearCookie('token', {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict'
+            sameSite:  process.env.NODE_ENV === 'production' ? 'none' : 'strict'
         });
 
         res.json({message: 'Logout'});
